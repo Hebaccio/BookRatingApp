41b7fc1e21f2bbfa6f068d34ef45146020c645ae
Hamza <hamza.hebibovic@edu.fit.ba>
2023-10-19 15:17:52 +0200

    Home page updated

--- BookiesFrontEnd/src/app/home/home.component.ts
+++ BookiesFrontEnd/src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  previousScrollPos: number = 0;
+  isFixed:boolean=true;
+
+  ngOnInit() {
+    this.isFixed = true;
+    this.previousScrollPos = window.scrollY;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const currentScrollPos = window.scrollY;
+
+    if (currentScrollPos > this.previousScrollPos) {
+      this.isFixed=false;
+    } else {
+      //this.renderer.setStyle(this.el.nativeElement.querySelector('.main-container'), 'opacity', 1);
+      this.isFixed=true;
+    }
+    this.previousScrollPos = currentScrollPos;
+  }
 
 }
--- BookiesFrontEnd/src/app/navbar/navbar.component.ts
+++ BookiesFrontEnd/src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit, Renderer2, ElementRef } from '@angular/core';
+import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
 import {Router} from "@angular/router";
 
 @Component({
@@ -6,28 +6,9 @@ import {Router} from "@angular/router";
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit{
-  constructor(private router: Router, private renderer:Renderer2, private el: ElementRef) {}
-  isFixed: boolean = true;
-  previousScrollPos: number = 0;
+export class NavbarComponent {
+  constructor(private router: Router, private renderer:Renderer2) {}
 
-  ngOnInit() {
-    this.isFixed = true;
-    this.previousScrollPos = window.scrollY;
-  }
-
-  @HostListener('window:scroll', ['$event'])
-  onScroll(event: Event): void {
-    const currentScrollPos = window.scrollY;
-
-    if (currentScrollPos > this.previousScrollPos) {
-      this.isFixed=false;
-    } else {
-      //this.renderer.setStyle(this.el.nativeElement.querySelector('.main-container'), 'opacity', 1);
-      this.isFixed=true;
-    }
-    this.previousScrollPos = currentScrollPos;
-  }
   OpenHome() {
     this.router.navigateByUrl("Home");
   }
