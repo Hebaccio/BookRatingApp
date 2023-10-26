13c8e4af9096d465ae0cc5251e3cf7f88e807bd9
Hamza <hamza.hebibovic@edu.fit.ba>
2023-10-26 17:22:32 +0200

    Navbar besides routing

--- BookiesFrontEnd/src/app/Top&Bottom/navbar/navbar.component.ts
+++ BookiesFrontEnd/src/app/Top&Bottom/navbar/navbar.component.ts
@@ -1,18 +1,71 @@
-import { Component, HostListener, OnInit, Renderer2 } from '@angular/core';
-import {Router} from "@angular/router";
+import { Component, HostListener, Renderer2, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-  constructor(private router: Router, private renderer:Renderer2) {}
+export class NavbarComponent implements OnInit{
 
-  OpenHome() {
-    this.router.navigateByUrl("Home");
+  previousScrollPos: number = 0;
+
+  constructor(private renderer:Renderer2, private el:ElementRef) {
   }
-  LogOut() {
-    this.router.navigateByUrl("Home");
+
+  ngOnInit() {
+    this.previousScrollPos = window.scrollY;
+  }
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    const currentScrollPos = window.scrollY;
+
+    if(window.innerWidth>900){
+      const scrollNavbar = this.el.nativeElement.querySelector('.ScrollNavbar');
+      const dropdownContent: null = this.el.nativeElement.querySelector('.dropdown-content');
+
+      //If we don't have login
+      if(dropdownContent!=null){
+        if (currentScrollPos > this.previousScrollPos) {
+          // Scrolling down
+          this.renderer.setStyle(dropdownContent, 'transition', '0.7s ease-in-out');
+          this.renderer.setStyle(scrollNavbar, 'top', '-180px');
+          this.renderer.setStyle(scrollNavbar, 'transition', '0.7s ease-in-out');
+        } else {
+          // Scrolling up
+          this.renderer.setStyle(scrollNavbar, 'top', '0px');
+          this.renderer.setStyle(scrollNavbar, 'transition', '0.7s ease-in-out');
+        }
+      } else { //If we have login
+        if (currentScrollPos > this.previousScrollPos) {
+          // Scrolling down
+          this.renderer.setStyle(scrollNavbar, 'top', '-180px');
+          this.renderer.setStyle(scrollNavbar, 'transition', '0.7s ease-in-out');
+        } else {
+          // Scrolling up
+          this.renderer.setStyle(scrollNavbar, 'top', '0px');
+          this.renderer.setStyle(scrollNavbar, 'transition', '0.7s ease-in-out');
+        }
+      }
+
+    }
+    else{
+      const top = this.el.nativeElement.querySelector('.Top');
+      const scrollNavbar = this.el.nativeElement.querySelector('.ScrollNavbar');
+      const dropdownContent: null = this.el.nativeElement.querySelector('.dropdown-content');
+
+      if (currentScrollPos > this.previousScrollPos) {
+        // Scrolling down
+        this.renderer.setStyle(top, 'top', '-125px');
+        this.renderer.setStyle(scrollNavbar, 'bottom', '-180px');
+        this.renderer.setStyle(top, 'transition', '0.7s ease-in-out');
+        this.renderer.setStyle(scrollNavbar, 'transition', '0.7s ease-in-out');
+      } else {
+        // Scrolling up
+        this.renderer.setStyle(top, 'top', '0px');
+        this.renderer.setStyle(scrollNavbar, 'bottom', '0px');
+      }
+    }
+
+    this.previousScrollPos = currentScrollPos;
   }
 }
